a119590dda3bb8ba2da001aa2e88a2ad576e22e7
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2021-06-18 20:59:09 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,58 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const SemverWebpackPlugin = require('semver-extended-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const SemverWebpackPlugin = require("semver-extended-webpack-plugin");
 
 module.exports = {
-    mode: 'production', // "production" | "development" | "none"
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    module: {
-        rules: [{
-            test: /\.m?(js|jsx)$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', { targets: "defaults" }]
-                    ],
-                    plugins: ['@babel/plugin-proposal-class-properties']
-                }
-            }
-        }]
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        publicPath: 'auto'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './public/index.html'
-        }),
-        new SemverWebpackPlugin({
-            files: [path.resolve(__dirname, 'package.json')],
-            incArgs: ['patch'],
-            console: true,
-            buildDate: true,
-            version: '0.0.1' // optional if you want to set the desired version
-        })
+  mode: "production", // "production" | "development" | "none"
+  entry: path.resolve(__dirname, "src", "index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-syntax-dynamic-import",
+            ],
+          },
+        },
+      },
     ],
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, 'build'),
-        port: 9000,
-        compress: true,
-        overlay: true,
-        open: true,
-        hot: true
-    }
-}
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    publicPath: "auto",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./public/index.html",
+    }),
+    new SemverWebpackPlugin({
+      files: [path.resolve(__dirname, "package.json")],
+      incArgs: ["patch"],
+      console: true,
+      buildDate: true,
+      version: "0.0.1", // optional if you want to set the desired version
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "build"),
+    port: 9000,
+    compress: true,
+    overlay: true,
+    open: true,
+    hot: true,
+  },
+};

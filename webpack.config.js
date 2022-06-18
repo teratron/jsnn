31604073fd56c0975b86569270a4ca2d4a29cc6a
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-06-18 10:28:40 +0300

    Update config webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,12 @@
 const dev = require('./config/webpack.dev')
 const prod = require('./config/webpack.prod')
 
-module.exports = (_env, args) => {
-    switch (args.mode) {
-        case 'none':
+module.exports = (_env, argv) => {
+    switch (argv.mode) {
+        default:
         case 'development':
             return dev
         case 'production':
             return prod
-        default:
-            throw new Error('No matching configuration was found!')
     }
 }
